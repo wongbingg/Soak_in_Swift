5fb6115f77d95e63a2ed767ad105a2fe9358fa01
wongbingg <wb277@naver.com>
2021-12-24 17:45:15 +0900

    clone

---
+++ aa.playground/Contents.swift
@@ -0,0 +1,13 @@
+
+func greeting1() {
+    print("Hello")
+    func greeting2(){
+        print("Hey")
+        
+    }
+    
+}
+
+func greeting3() {
+    
+}
---
+++ ifelse,switch.playground/Contents.swift
@@ -0,0 +1,30 @@
+//func loveCalculator() {
+//    let loveScore = Int.random(in: 0...100)
+//
+//    if loveScore > 80 {
+//        print("You love each other like Kanye loves Kanye.")
+//    }else if loveScore > 40 {
+//        print("you go together like Coke and Mentos")
+//    }else {
+//        print("You'll be forever alone")
+//    }
+//}
+//
+//loveCalculator()
+
+
+func loveCalculator() {
+    let loveScore = Int.random(in: 0...100)
+    
+    switch loveScore {
+    case 81...100:
+        print("You love each other like Kanye loves Kanye.")
+    case 41..<81:
+        print("you go together like Coke and Mentos")
+    case ...40:
+        print("You'll be forever alone")
+    default:
+        print("Error lovescore is out of range.")
+    }
+}
+loveCalculator()
