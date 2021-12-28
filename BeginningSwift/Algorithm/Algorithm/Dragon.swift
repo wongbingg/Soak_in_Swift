c878646cad0c53896ba5d85d1efa1ceb56ca6d03
wongbingg <wb277@naver.com>
2021-12-28 23:17:52 +0900

    new cloneBMI

---
+++ BeginningSwift/Algorithm/Algorithm/Dragon.swift
@@ -0,0 +1,14 @@
+//class Dragon: Enemy {
+//    var wingSpan = 2
+//    
+//    func talk(speech: String) {
+//        print("Says: \(speech)")
+//    }
+//    override func move() {  // move() == from Enemy Class so, override
+//        print("Fly forward")
+//    }
+//    override func attack() {
+//        super.attack()
+//        print("Spits fire, does 10 damage")
+//    }
+//}
--- BeginningSwift/Algorithm/Algorithm/main.swift
+++ BeginningSwift/Algorithm/Algorithm/main.swift
@@ -1,18 +1,11 @@
-//
-//  main.swift
-//  Algorithm
-//
-//  Created by 이원빈 on 2021/12/25.
-//
-import Foundation
-let a = readLine()!.components(separatedBy: " ").map{Int($0)!}
 
-var A:Int = (a[0] * (a[1] % 10))
-var B:Int = (a[0] * ((a[1] % 100)/10))
-var C:Int = (a[0] * (a[1] / 100))
-var D:Int = (a[0] * a[1])
+var skeleton1 = Enemy(health: 100, attackStrength: 10)
+var skeleton2 = skeleton1
 
-print(A)
-print(B)
-print(C)
-print(D)
+skeleton1.takeDamage(amount: 10)
+skeleton1.takeDamage(amount: 10)
+skeleton2.takeDamage(amount: 10)
+
+print(skeleton1.health)
+print(skeleton2.health)
+
