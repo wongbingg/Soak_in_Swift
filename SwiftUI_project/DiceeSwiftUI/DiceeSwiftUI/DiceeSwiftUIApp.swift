760a5a2f19fe85ec014057329273959eb1e14bca
wongbingg <wb277@naver.com>
2022-01-15 17:13:00 +0900

    one folder

---
+++ SwiftUI_project/DiceeSwiftUI/DiceeSwiftUI/DiceeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeSwiftUIApp.swift
+//  DiceeSwiftUI
+//
+//  Created by 이원빈 on 2022/01/15.
+//
+
+import SwiftUI
+
+@main
+struct DiceeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
