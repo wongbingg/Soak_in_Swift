02335b99d5648b3e86ec1ccd880bc1d99318299d
wongbingg <wb277@naver.com>
2022-01-29 23:30:05 +0900

    advanced calculate app challenge

--- BeginningSwift/ifelse,switch.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ BeginningSwift/ifelse,switch.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -1,17 +1,3 @@
-//func loveCalculator() {
-//    let loveScore = Int.random(in: 0...100)
-//
-//    if loveScore > 80 {
-//        print("You love each other like Kanye loves Kanye.")
-//    }else if loveScore > 40 {
-//        print("you go together like Coke and Mentos")
-//    }else {
-//        print("You'll be forever alone")
-//    }
-//}
-//
-//loveCalculator()
-
 
 func loveCalculator() {
     let loveScore = Int.random(in: 0...100)
