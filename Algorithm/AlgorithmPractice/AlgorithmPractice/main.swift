e2acd32aea6cbe4cf9d87f693d3b942b9988da04
wongbingg <wb277@naver.com>
2022-04-03 18:55:31 +0900

    알고리즘 연습

--- Algorithm/AlgorithmPractice/AlgorithmPractice/main.swift
+++ Algorithm/AlgorithmPractice/AlgorithmPractice/main.swift
@@ -1,26 +1,17 @@
 //<2941> 크로아티아 알파벳
 
+//크로아티아 문자를 인식하고 1로 치환을 해준다
+//남은 문자는 .count 를 이용해 갯수를 세어준 후 cnt랑 합쳐준다
+
+import Foundation
+
 let input = readLine()!
-
-var croatiaChar:[String] = ["c=","c-","dz=","d-","lj","nj","s=","z="]
-var croa_1 = "c="
-var croatiaChar_Map:[Character] = croatiaChar.map{Character($0)}
-var count = 0
-
-var result = count + removeCroatiaLetter(input).count
-print(result)
-
-
-
-func removeCroatiaLetter(_ str: String) -> String {
-    var testString = str
-//    for cras in croatiaChar {
-//        if testString.contains(cras) {
-//            testString.remove(at: testString.firstIndex(of: cras)!)
-//            count += 1
-//        }
-//    }
-    testString.contains(croa_1)
-    return testString
-    
+var testInput = input
+var croatiaLetterList = ["c=","c-","dz=","d-","lj","nj","s=","z="]
+for i in croatiaLetterList {
+    if testInput.contains(i){
+        testInput = testInput.replacingOccurrences(of: i, with: "1")
+    }
 }
+print(testInput.count)
+
