1c4d461bf83db9572cf744f6a903de807fce5f22
웡빙 <wb277@naver.com>
2022-05-05 17:06:34 +0900

    오토레이아웃 Simple Constraints

---
+++ BeginningSwift/autoLayout/autoLayout/MyView.swift
@@ -0,0 +1,16 @@
+//
+//  MyView.swift
+//  autoLayout
+//
+//  Created by 이원빈 on 2022/05/05.
+//
+
+import UIKit
+
+@IBDesignable
+class MyView: UIView {
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 50, height: 50)
+    }
+}
---
+++ BeginningSwift/autoLayout_Instrinsic/autoLayout_Instrinsic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autoLayout_Instrinsic
+//
+//  Created by 이원빈 on 2022/05/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
