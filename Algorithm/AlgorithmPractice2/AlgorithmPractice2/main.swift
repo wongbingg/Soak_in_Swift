90de72575857899b811866edc2bc72d3461d5a47
웡빙 <wb277@naver.com>
2022-05-24 00:13:33 +0900

    algorithm : 유클리드 호제법

--- Algorithm/AlgorithmPractice2/AlgorithmPractice2/main.swift
+++ Algorithm/AlgorithmPractice2/AlgorithmPractice2/main.swift
@@ -1,31 +1,32 @@
-// <15829> Hashing
-// 마치 31진수를 나타내는 과정 같다.
-// abcde 의 해시값은 1xpow(31,0) + 2xpow(31,1) + 3xpow(31,2) + 4xpow(31,3)
-//예제 1: abcde의 해시 값은 1 × 310 + 2 × 311 + 3 × 312 + 4 × 313 + 5 × 314 = 1 + 62 + 2883 + 119164 + 4617605 = 4739715이다.
-//예제 2: zzz의 해시 값은 26 × 310 + 26 × 311 + 26 × 312 = 26 + 806 + 24986 = 25818이다.
-//import Foundation
-//
-let mod = 1234567891
-var L = readLine()
-let englishInput = Array(readLine()!).map{ Int($0.asciiValue! - 96) }
-var result = 0
-var m = 1
-for i in englishInput {
-    result = (result + (i * m)) % mod
-    m = (m * 31) % mod
+//<2609> 최대공약수와 최소공배수
+// 두 개의 자연수를 입력받아 최대 공약수와 최소 공배수를 출력하는 프로그램을 작성
+// 10,000 이하의 자연수
+// 유클리드 호제법
+let input = readLine()!.split(separator: " ").map{ Int($0)! }
+var n = input
+var maxGongyaksu: Int = 0
+var minGongbaesu: Int = 0
+var cnt = 0
+while true {
+    cnt += 1
+    n = n.sorted()
+    let result = n[1] % n[0]
+    if cnt == 1 {
+        if result == 0 {
+            maxGongyaksu = n[0]
+            minGongbaesu = n[1]
+            break
+        }
+    }
+    if result == 0 {
+        maxGongyaksu = n[0]
+        break
+    } else {
+        n[1] = result
+    }
 }
-print(result)
 
-// 위 코드는 구글링 후 블로그 코드
-
-
-//let mod = 1234567891
-//readLine()
-//let input = Array(readLine()!).map{Int(exactly: $0.asciiValue!)! - 96}
-//var hash = 0
-//var m = 1
-//for i in input{
-//    hash = (hash + i*m) % mod
-//    m = (m*31) % mod
-//}
-//print(hash)
+var a = input.max()! / maxGongyaksu
+minGongbaesu = a * input.min()!
+print(maxGongyaksu)
+print(minGongbaesu)
