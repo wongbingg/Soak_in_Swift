6b1f88e89a06dd180b2c6398e0db3c4acd796b5e
웡빙 <wb277@naver.com>
2022-08-19 17:06:44 +0900

    algo : 비트마스크 문제진행

--- Algorithm/AlgorithmPractice2/AlgorithmPractice2/main.swift
+++ Algorithm/AlgorithmPractice2/AlgorithmPractice2/main.swift
@@ -1,31 +1,86 @@
 //<11723> 집합
+import Foundation
 
-let M = Int(readLine()!)!
-var S = Set<Int>()
-var resultList = [Int]()
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+let FIO = FileIO()
+let M = FIO.readInt()
+var S:UInt32 = 0
+var resultList = [String]()
 (1...M).forEach { _ in
-    guard let order = readLine()?.split(separator: " ") else { return }
+    let order = FIO.readString().split(separator: " ")
     var number = 0
     if order.count == 2 {
         number = Int(order[1])!
     }
     switch order[0] {
     case "add":
-        S.update(with: number)
+        S |= (1<<(number-1))
     case "remove":
-        S.remove(number)
+        S -= S & (1<<(number-1))
     case "check":
-        S.contains(number) ? resultList.append(1) : resultList.append(0)
+        resultList.append(String((S & (1<<(number-1)))>>(number-1), radix: 2))
     case "toggle":
-        if S.contains(number) {
-            S.remove(number)
-        } else {
-            S.update(with: number)
-        }
+        S = S ^ (1<<(number-1))
     case "all":
-        S = Set<Int>(1...20)
+        S = 0b11111111111111111111
     case "empty":
-        S.removeAll()
+        S = 0
     default:
         return
     }
